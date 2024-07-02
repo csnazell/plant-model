7a663abccff388c939d581d8030792742e49b099
Christopher Snazell <chris@snazell.software>
2024-07-02 13:10:55 +0100

    Added clock adapter for F2014 Red model

--- PlantModelFramework/src/Phenologies/ClockAdapters/F2014.jl
+++ PlantModelFramework/src/Phenologies/ClockAdapters/F2014.jl
@@ -11,7 +11,56 @@
 
 module F2014
 
-    module COP1
+module Common
+
+    # dependencies ------------------------------------------------------------
+    
+    # standard library
+    # -
+
+    # third-party
+    # - 
+
+    # package
+    
+    import ....PIFCOFT
+
+    # local
+    # -
+
+    # implementation ----------------------------------------------------------
+
+    function createClockInput(T, U, parameters)
+        
+        # clock model properties
+
+        LUXp  = U[:,23] 
+        NOXp  = U[:,33] 
+        ELF34 = U[:,21] 
+        ELF3p = U[:,20] 
+
+        # phenology clock inputs
+
+        cP      = U[:,5] 
+        COP1n_n = U[:,25] 
+        EC      = ( (LUXp .+ parameters.f6 * NOXp) .* (ELF34 .+ parameters.f1 * ELF3p) ) ./ 
+                   ( 1.0 .+ parameters.f3 * (LUXp + parameters.f2 * NOXp) + parameters.f4 * (ELF34 + parameters.f1 * ELF3p) )
+        GIn     = U[:,31] * 40.9 
+        LHY     = ( U[:,2] + U[:,4] ) / 1.561   # LHY + CCA1
+        PRR5    = U[:,12] * 0.841               # nuclear
+        PRR7    = U[:,9] / 2.6754 
+        PRR9    = U[:,7] 
+        TOC1    = U[:,14] * 1.21                # nuclear
+
+        # clockinput
+
+        PIFCOFT.ClockInput(cP, COP1n_n, EC, GIn, LHY, PRR5, PRR7, PRR9, TOC1, T)
+
+    end
+
+end # end: module: Common
+
+module COP1
 
     # dependencies ------------------------------------------------------------
     
@@ -27,6 +76,10 @@ module F2014
     import ......Clocks.F2014 as F2014
     import ......Phenology
     import ....PIFCOFT
+
+    # local
+
+    import ..Common: createClockInput
 
     # implementation ----------------------------------------------------------
 
@@ -48,35 +101,57 @@ module F2014
 
     function (a::Phenology.ClockOutputAdapter{<: F2014.COP1.DynamicsParameters})(clockOutput::Clock.Output)::PIFCOFT.ClockInput
 
-        Umatrix = vcat(clockOutput.U...)
-
-        # clock model properties
-
-        LUXp  = Umatrix[:,23] 
-        NOXp  = Umatrix[:,33] 
-        ELF34 = Umatrix[:,21] 
-        ELF3p = Umatrix[:,20] 
-
-        # phenology clock inputs
-
-        cP      = Umatrix[:,5] 
-        COP1n_n = Umatrix[:,25] 
-        EC      = ( (LUXp .+ a.parameters.f6 * NOXp) .* (ELF34 .+ a.parameters.f1 * ELF3p) ) ./ 
-                   ( 1.0 .+ a.parameters.f3 * (LUXp + a.parameters.f2 * NOXp) + a.parameters.f4 * (ELF34 + a.parameters.f1 * ELF3p) )
-        GIn     = Umatrix[:,31] * 40.9 
-        LHY     = ( Umatrix[:,2] + Umatrix[:,4] ) / 1.561   # LHY + CCA1
-        PRR5    = Umatrix[:,12] * 0.841                           # nuclear
-        PRR7    = Umatrix[:,9] / 2.6754 
-        PRR9    = Umatrix[:,7] 
-        TOC1    = Umatrix[:,14] * 1.21                            # nuclear
-        T       = clockOutput.T
-
-        # clockinput
-
-        PIFCOFT.ClockInput(cP, COP1n_n, EC, GIn, LHY, PRR5, PRR7, PRR9, TOC1, T)
+        return createClockInput(clockOutput.T, vcat(clockOutput.U...), a.parameters)
 
     end
 
-    end #end: module: COP1
+end #end: module: COP1
+
+module Red
+
+    # dependencies ------------------------------------------------------------
+    
+    # standard library
+    # -
+
+    # third-party
+    # - 
+
+    # package
+    
+    import ......Clock
+    import ......Clocks.F2014 as F2014
+    import ......Phenology
+    import ....PIFCOFT
+
+    # local
+
+    import ..Common: createClockInput
+
+    # implementation ----------------------------------------------------------
+
+    #
+    # Red.DynamicsParameters -> PIFCOFT.ClockInput
+    #
+    
+    # functions
+
+    # - factory
+    
+    function pifcoftAdapter(parameters::F2014.Red.DynamicsParameters)
+
+        Phenology.ClockOutputAdapter(parameters)
+
+    end
+
+    # - type-specific adapter implementation 
+
+    function (a::Phenology.ClockOutputAdapter{<: F2014.Red.DynamicsParameters})(clockOutput::Clock.Output)::PIFCOFT.ClockInput
+
+        return createClockInput(clockOutput.T, vcat(clockOutput.U...), a.parameters)
+
+    end
+
+end #end: module: Red
 
 end #end: module: F2014
